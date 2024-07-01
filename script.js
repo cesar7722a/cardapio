93d998bdd66077081549b4310c345f77136ff4a9
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-07-01 19:07:25 +0100

    finalizando a funcionalidade de adicionar no carrinho

--- script.js
+++ script.js
@@ -117,6 +117,47 @@ function removeItemCart(name) {
     cart.splice(index, 1)
     updateCartModal()
   }
+}
 
+addressInput.addEventListener("input", function (event) {
+  let inputValue = event.target.value;
 
+  if (inputValue !== "") {
+    addressInput.classList.remove("border-red-500")
+    addressWarn.classList.add("hidden")
+  }
+
+})
+
+checkoutBtn.addEventListener("click", function () {
+
+  const isOpen = checkRestaurantOpen()
+  if (!isOpen) {
+    alert("RESTAURANTE FECHADO NO MOMENTO!")
+    return
+  }
+
+  if (cart.length === 0) return;
+  if (addressInput.value === "") {
+    addressWarn.classList.remove("hidden")
+    addressInput.classList.add("border-red-500")
+    return
+  }
+})
+
+function checkRestaurantOpen() {
+  const data = new Date()
+  const hora = data.getHours()
+  return hora >= 18 && hora <= 22
+}
+
+const spanItem = document.getElementById("date-span")
+const isOpen = checkRestaurantOpen()
+
+if (isOpen) {
+  spanItem.classList.remove("bg-red-500")
+  spanItem.classList.add("bg-green-600")
+} else {
+  spanItem.classList.remove("bg-green-600")
+  spanItem.classList.add("bg-red-500")
 }
